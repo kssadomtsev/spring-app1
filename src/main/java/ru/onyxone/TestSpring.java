60486e445537e82fddd71395dfb7621700737396
Konstantin <sadomcev.k@gmai.com>
2021-01-02 13:32:43 +0300

    Video 2

--- src/main/java/ru/onyxone/TestSpring.java
+++ src/main/java/ru/onyxone/TestSpring.java
@@ -6,7 +6,9 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         TestBean testBean = context.getBean("testBean", TestBean.class);
+        TestBean testBean2 = context.getBean("testBean", TestBean.class);
         System.out.println(testBean.getName());
+        System.out.println(testBean2.getName());
         context.close();
     }
 }
