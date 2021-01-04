1b7f8d8f0905a856968a70ecaad05ec7cf72c32a
Konstantin <sadomcev.k@gmai.com>
2021-01-04 17:58:36 +0300

    Lesson 11 half

--- src/main/java/ru/onyxone/MusicPlayer.java
+++ src/main/java/ru/onyxone/MusicPlayer.java
@@ -1,6 +1,7 @@
 package ru.onyxone;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -8,37 +9,16 @@ import java.util.List;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-    private String name;
-    private int volume;
-
-    public MusicPlayer() {
-    }
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1, @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
-        return classicalMusic.getSong();
+        return music1.getSong() + ", " + music2.getSong();
     }
 }
